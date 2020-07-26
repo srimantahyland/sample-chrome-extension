27946133e4de2f800404ce560f45b036ef086e4f
srimantahyland <srimanta.singh@hyland.com>
2020-07-26 23:17:42 +0530

    Adding initial files

---
+++ BudgetManager/options.js
@@ -0,0 +1,18 @@
+$(function () {
+    chrome.storage.sync.get("limit", function (budget) {
+        if (budget.limit) {
+            $("#limit").val(budget.limit);
+        }
+    });
+    $("#saveLimit").click(function () {
+        var limit = $("#limit").val();
+        if (limit) {
+            chrome.storage.sync.set({ "limit": limit });
+            close();
+        }
+    });
+    $("#resetTotal").click(function () {
+        chrome.storage.sync.set({ "total": 0 });
+    });
+
+});
